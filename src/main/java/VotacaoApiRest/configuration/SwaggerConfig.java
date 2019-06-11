ec1ad8842d87bbe940c853f0663432464c9f5d04
kellensd <kellensd@hotmail.com>
2019-06-10 23:13:57 -0300

    Ajuste do pom. Ajuste swagger.

--- src/main/java/VotacaoApiRest/configuration/SwaggerConfig.java
+++ src/main/java/VotacaoApiRest/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package VotacaoApiRest.application;
+package VotacaoApiRest.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
