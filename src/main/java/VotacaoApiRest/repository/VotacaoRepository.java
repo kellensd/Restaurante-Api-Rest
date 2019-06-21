0148fe6b25432d2c50399736713e6a96a61273ef
kellensd <kellensd@hotmail.com>
2019-06-20 23:40:33 -0300

    Adicionado busca por id no VotacaoRepository

--- src/main/java/VotacaoApiRest/repository/VotacaoRepository.java
+++ src/main/java/VotacaoApiRest/repository/VotacaoRepository.java
@@ -3,10 +3,13 @@ package VotacaoApiRest.repository;
 import VotacaoApiRest.domain.model.Votacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface VotacaoRepository extends JpaRepository<Votacao, Long> {
 
     List<Votacao> findAll();
+    Optional<Votacao> findById(Long id);
 }
