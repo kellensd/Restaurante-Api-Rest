78d2173bb770624d72b592e34397491ed98e4ba9
kellensd <kellensd@hotmail.com>
2021-02-26 19:53:06 -0300

    Ajustado as configurações de conexão com o database no application.yml Criado um novo arquivo data.sql Excluído os demais arquivos data-h2.sql e schema-h2.sql

--- src/main/resources/schema-h2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE VOTACAO(ID BIGINT PRIMARY KEY AUTO_INCREMENT,
-                  nomeRestaurante VARCHAR(50) NOT NULL,
-                  dataVotacao DATE NOT NULL DEFAULT SYSDATE,
-                  nomeProfissional VARCHAR(50) NOT NULL,
-                  voto INT,
-                  descricao VARCHAR(250)
-                  );
