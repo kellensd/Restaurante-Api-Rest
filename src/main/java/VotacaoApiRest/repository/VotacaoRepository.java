276c54afc26d601200e95a553da95144018f5914
kellensd <kellensd@hotmail.com>
2021-02-27 14:02:24 -0300

    criado a consulta "findRestaurantesVotados" via jpa.

--- src/main/java/VotacaoApiRest/repository/VotacaoRepository.java
+++ src/main/java/VotacaoApiRest/repository/VotacaoRepository.java
@@ -2,9 +2,11 @@ package VotacaoApiRest.repository;
 
 import VotacaoApiRest.domain.model.Votacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,7 @@ public interface VotacaoRepository extends JpaRepository<Votacao, Long> {
 
     List<Votacao> findAll();
     Optional<Votacao> findById(Long id);
+
+    @Query(value = "SELECT NOME_RESTAURANTE as restaurante, count(VOTO) as votos FROM VOTACAO group by NOME_RESTAURANTE ORDER BY VOTOS DESC", nativeQuery = true)
+    List<Map<String, String>> findRestaurantesVotados();
 }
