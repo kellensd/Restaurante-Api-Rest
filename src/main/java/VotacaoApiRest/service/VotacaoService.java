63d34ed2fe72b98f672e76d429a08d410f52c2b6
kellensd <kellensd@hotmail.com>
2021-02-27 13:58:02 -0300

    Ajustado a nomenclatura do método

--- src/main/java/VotacaoApiRest/service/VotacaoService.java
+++ src/main/java/VotacaoApiRest/service/VotacaoService.java
@@ -10,6 +10,6 @@ public interface VotacaoService {
 
     List<Votacao> findAll();
     Votacao findById(Long id);
-    List<Map<String, String>> findMaisVotados();
+    List<Map<String, String>> findRestaurantesVotados();
     void votar(ComandoVotar votacao);
 }
